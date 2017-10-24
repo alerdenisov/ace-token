05829f3c198f99f3b0d93f81a564222e5ea83ee0
Aler Denisov <aler.zampillo@gmail.com>
2017-10-24 18:59:26 +0300

    distribution tests

--- test/acedistribution.js
+++ test/acedistribution.js
@@ -17,7 +17,49 @@ contract('AceTokenDistribution', accounts => {
 
   describe('Initialization', async() => {
     it('should be deploeyd', async() => {
-      assert(DIST)
+      assert(DISTR)
+    })
+
+    it('should haven\'t right to mint by default', async() => {
+      expectThrow(DISTR.bulkMint(accounts, accounts.map(acc => 100000), OWNER_SIGNATURE))
+    })
+
+    it('should give right to mint', async() => {
+      await ACE.transferOwnership(DISTR.address, OWNER_SIGNATURE);
+      await DISTR.bulkMint(accounts, accounts.map(acc => 100000), OWNER_SIGNATURE);
+    })
+
+    it('should return rights to mint', async() => {
+      await ACE.transferOwnership(DISTR.address, OWNER_SIGNATURE);
+      assert(await ACE.owner() == DISTR.address);
+      await DISTR.returnOwnership(OWNER_SIGNATURE);
+      assert(await ACE.owner() == accounts[0]);
+    })
+  })
+
+  describe('Minting', async() => {
+    const assertEquals = async (func, expect, msg) => {
+      const value = await func()
+      assert(value.equals(expect), msg || `Value isnt expected! (current is ${value} but expected is ${expect})`)
+    }
+
+    const assertSupply = async (expect, msg) => {
+      await assertEquals(ACE.totalSupply, expect, msg)
+    }
+
+    beforeEach(async() => {
+      await ACE.transferOwnership(DISTR.address, OWNER_SIGNATURE)
+    })
+
+    it('should mint 10000 tokens', async() => {
+      await DISTR.bulkMint([accounts[0], accounts[1]], [5000,5000], OWNER_SIGNATURE)
+      assertSupply(10000)
+    })
+
+    it('should mint 6000+4000 tokens', async() => {
+      await DISTR.bulkMint([accounts[0], accounts[1]], [3000,3000], OWNER_SIGNATURE)
+      await DISTR.extraMint(OWNER_SIGNATURE)
+      assertSupply(10000)
     })
   })
 })
