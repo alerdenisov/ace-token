0e8c984051436b4cb38e8dd42415ef9a5519bbd2
Aler Denisov <aler.zampillo@gmail.com>
2017-10-31 13:48:16 +0300

    change license headers

--- contracts/AceTokenDistribution.sol
+++ contracts/AceTokenDistribution.sol
@@ -1,7 +1,6 @@
 // ACE Token is a first token of Token Stars platform
-
-// Copyright (c) 2017 Aler Denisov <aler.zampillo@gmail.com>
-
+// Copyright (c) 2017 TokenStars
+// Made by Aler Denisov
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
